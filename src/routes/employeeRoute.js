a4607caef28f1f677626f222733c84af3fd004c4
Rafik <rfkabubakari@gmail.com>
2024-01-08 14:31:10 +0000

    Get Employeee created task for today

--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -1,7 +1,7 @@
 // src/routes/employee.js
 const express = require("express");
 const router = express.Router();
-const { loginEmployee, clockin, clockOut, getSingleEmployeeAttendancRecords, createTask, getEmployeeTaskHistory, editTaskReport, deleteATaskReport, leaveRequest, editLeaveRequest, deleteALeaveRequest } = require('../controllers/empolyeeController')
+const { loginEmployee, clockin, clockOut, getSingleEmployeeAttendancRecords, createTask, getEmployeeTaskHistory, editTaskReport, deleteATaskReport, leaveRequest, editLeaveRequest, deleteALeaveRequest, getTodayCreatedTask } = require('../controllers/empolyeeController')
 
 // Import the Employee model
 
@@ -22,6 +22,9 @@ router.get("/attendance/:employeeId", getSingleEmployeeAttendancRecords)
 router.post("/create-task", createTask)
 //Get employee Task Task History
 router.get("/task/:employeeId",  getEmployeeTaskHistory)
+
+//Get todays created task
+router.get("/task/today/:employeeId", getTodayCreatedTask)
 
 // Edit a task report
 router.patch("/task/edit/:reportId", editTaskReport)
