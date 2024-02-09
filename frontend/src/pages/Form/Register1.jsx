ccae576774413d3a734dd977a1a0742437b0d237
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2024-02-09 18:30:11 +0000

    updated the feature for checking password criteria

--- frontend/src/pages/Form/Register1.jsx
+++ frontend/src/pages/Form/Register1.jsx
@@ -3,8 +3,11 @@ import './Register.css'
 import PasswordCheck from './passwordFeatures/PasswordCheck';
 
 export default function Register1({ name, email, staffId }) {
-  const [phase1, setPhase1] = useState(false) 
-  const [phase2, setPhase2] = useState(false) 
+  // const [password, setPassword] = useState('');
+  const [isValidLength, setIsValidLength] = useState(false);
+  const [hasUpperAndLower, setHasUpperAndLower] = useState(false);
+  const [hasAnumber, setHasAnumber] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
   // const [form, setForm] = useState({
   //   name: name,
   //   email: email,
@@ -13,27 +16,17 @@ export default function Register1({ name, email, staffId }) {
   //   password1: ''
   // });
 
-  const plain = useRef(new PasswordCheck())
-
   const handleChange = (e) => {
-    plain.current.add(e.target.value)
-    const eightOrMore = plain.current.isEightOrMore();
-    console.log(eightOrMore)
-    if(eightOrMore){
-      setPhase1(true)
-      console.log('characters are 8 or more')
-      plain.current.plaintext = [];
-    } else {
-      setPhase1(false);
-    }
+    const input = e.target.value
 
-    const UpperAndLower = plain.current.containsUpperAndLower()
-    if(UpperAndLower){
-      setPhase2(true);
-    } else {
-      setPhase2(false);
-    }
+    const checker = new PasswordCheck();
+    checker.add(input)
 
+    setIsValidLength(checker.isEightOrMore());
+    setHasUpperAndLower(checker.containsUpperAndLower());
+    setHasAnumber(checker.constainsAnumber());
+    setSpecialChar(checker.containsAspecialChar());
+    
   }
 
   return (
@@ -42,10 +35,10 @@ export default function Register1({ name, email, staffId }) {
         <input name='password' type='password' placeholder='create a password' className='Input-R1' required onChange={handleChange} />
         <input name='password1' type='password' placeholder='confirm password' className='Input-R1' required />
         <ul className='password-Check'>
-            <li className={phase1 ? 'pass-text': 'wrong-text'}>Password must be at least 8 characters</li>
-            <li className={phase2 ? 'pass-text': 'wrong-text'}>Password must contain an uppercase a lowercase letter</li>
-            <li className='wrong-text'>Password must contain a number</li>
-            <li className='wrong-text'>Must contain a special case character e.g(@.$.#.%...)</li>
+            <li className={isValidLength ? 'pass-text': 'wrong-text'}>Password must be at least 8 characters</li>
+            <li className={hasUpperAndLower ? 'pass-text': 'wrong-text'}>Password must contain an uppercase a lowercase letter</li>
+            <li className={hasAnumber ? 'pass-text' : 'wrong-text'}>Password must contain a number</li>
+            <li className={specialChar ? 'pass-text' : 'wrong-text'}>Must contain a special case character e.g(@.$.#.%...)</li>
         </ul>
         <button className="submit-btn">Submit</button>
     </form>
